0c718913ea585b59964798bda756b66ee6a334d7
learnlong <28563410399@qq.com>
2018-08-14 10:46:32 +0800

    ognl........

--- WebContent/HelloWorld.jsp
+++ WebContent/HelloWorld.jsp
@@ -5,6 +5,8 @@
 <title>Hello World</title>
 </head>
 <body>
-   Hello World, <s:property value="name"/>
+   Entered value : <s:property value="name"/><br/>
+   Value of key 1 : <s:property value="key1" /><br/>
+   Value of key 2 : <s:property value="key2" /> <br/>
 </body>
 </html>
--- src/cn/w3cschool/struts2/HelloWorldAction.java
+++ src/cn/w3cschool/struts2/HelloWorldAction.java
@@ -1,13 +1,25 @@
 package cn.w3cschool.struts2;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.enterprise.inject.New;
+
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.util.ValueStack;
 
 public class HelloWorldAction extends ActionSupport{
 	private String name;
 
 	 public String execute() throws Exception {
-		 System.out.println("Inside action....");
-	      return "success";
+		 ValueStack stack = ActionContext.getContext().getValueStack();
+		 Map<String, Object> context = new HashMap<String,Object>();
+		 context.put("key1", new String("This is key1"));
+		 context.put("key2", new String("This is key2"));
+		 stack.push(context);
+		 System.out.println("Size of the valueStack" + stack.size());
+		 return "success";
 	   }
 	   
 	   
